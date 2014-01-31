8d31a93f51fe86e1569825dec5287b04d95526e3
permyak <nextzucker@gmail.com>
2014-02-01 00:47:49 +0600

    add sign up page test

---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe "User pages" do
+
+  subject { page }
+
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_content('Sign up') }
+    it { should have_title('Who Are Better?') }
+  end
+  describe "profile page" do
+  # Replace with code to make a user variable
+  before { visit user_path(user) }
+
+  it { should have_content(user.name) }
+  it { should have_title(user.name) }
+end
+end
